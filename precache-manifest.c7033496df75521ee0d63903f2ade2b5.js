6f518e6aef052fa09b2c9bc1211f953fe89beeb9
Nirbhay Pherwani <pherwani37@gmail.com>
2020-03-29 17:01:48 -0400

    Updates

---
+++ precache-manifest.c7033496df75521ee0d63903f2ade2b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14f9072834603677b80dc9d56d77e16",
+    "url": "/spothole.authority/index.html"
+  },
+  {
+    "revision": "05d9aa0fb6235e7e4955",
+    "url": "/spothole.authority/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "76d9841aeeb035097493",
+    "url": "/spothole.authority/static/css/main.fc01929c.chunk.css"
+  },
+  {
+    "revision": "05d9aa0fb6235e7e4955",
+    "url": "/spothole.authority/static/js/2.58690e7f.chunk.js"
+  },
+  {
+    "revision": "e91f1f731fb5920a53e3269af7727f4d",
+    "url": "/spothole.authority/static/js/2.58690e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d9841aeeb035097493",
+    "url": "/spothole.authority/static/js/main.9d1c2115.chunk.js"
+  },
+  {
+    "revision": "e4449a0383e5c85e5abc",
+    "url": "/spothole.authority/static/js/runtime-main.88b3e7ad.js"
+  }
+]);
